62b7f13eb8e5b6ac7c334c5a57a1119922bdbac8
Chris Veness <chrisv@movable-type.co.uk>
2016-11-10 11:14:25 +0000

    Koa v2

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Koa *hello world* app
 One step up from Koa’s [hello world](http://koajs.com#application), this is a very simple Koa app
 using routing and handlebars templating.
 
-There is a Koa v2 version (using async/await) on the [koa-v2](tree/koa-v2) branch.
+This is a Koa v2 version (using async/await). This version requires Node.js v7.0.0+. There is a Koa 
+v1 version on the [master](../..) branch.
--- app.js
+++ app.js
@@ -2,21 +2,29 @@
 /* app.js; minimal 'hello world' app (using router & handlebars)                                  */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
-const koa    = require('koa');          // koa framework
+const Koa    = require('koa');          // koa framework
 const router = require('koa-router')(); // router middleware for koa
 const hbs    = require('koa-hbs');      // handlebars templating
+const co     = require('co');           // generator async control flow goodness
 
-const app = koa();
+const app = new Koa();
 
 app.use(hbs.middleware({
     extname:  '.html',
     viewPath: __dirname + '/',
 }));
+app.use(async function(ctx, next) { // for koa-hbs@0.9.0, convert ctx.render to return a promise
+    const render = ctx.render;
+    ctx.render = async function _convertedRender() {
+        return co.call(ctx, render.apply(ctx, arguments))
+    }
+    await next();
+});
 
-router.get('/', function*(next) {
+router.get('/', async function(ctx, next) {
     const context = { version: process.version, time: new Date() };
     console.log('router.get /', context);
-    yield this.render('hello-world', context);
+    await ctx.render('hello-world', context);
 });
 
 app.use(router.routes());
